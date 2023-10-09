81970c61953241cd2388be421bdc9f7f0c592192
Djeferson Luiz Kuhn Almeida <142161376+Djeferson-Almeida@users.noreply.github.com>
2023-10-09 20:09:23 -0300

    Add files via upload
    Exercício 2 aula Arduino

---
+++ ex_02.ino
@@ -0,0 +1,15 @@
+// C++ code
+//
+void setup()
+{
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop()
+{
+ delay(1000); // Aguarda 1000 milésimos de segundo
+ digitalWrite(LED_BUILTIN, LOW); // Desliga o LED
+ delay(1000); // Aguarda 1000 milésimos de segundo
+ digitalWrite(LED_BUILTIN, HIGH); // Liga o LED
+
+}
