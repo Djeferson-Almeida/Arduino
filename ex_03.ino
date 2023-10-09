5b09ada67afce2381a9efa8e8d31edd721da65a7
Djeferson Luiz Kuhn Almeida <142161376+Djeferson-Almeida@users.noreply.github.com>
2023-10-09 20:13:27 -0300

    Add files via upload
    Exercício 3 de Arduíno

---
+++ ex_03.ino
@@ -0,0 +1,15 @@
+int tempo = 0;
+void setup()
+{
+  pinMode(LED_BUILTIN, OUTPUT);
+  tempo = 1000;
+}
+
+void loop()
+{
+  delay(tempo);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(tempo);
+  digitalWrite(LED_BUILTIN, HIGH);
+
+}
